f561476c2afd9f20fa274a0d4b7714239fb18e18
Kapilktr <74752691+Kapilktr@users.noreply.github.com>
2023-08-09 11:51:47 +0530

    Update general.js
    Routes have been added

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,40 +4,91 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// Route for registering a new user
+public_users.post("/register", (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).json({ message: "Username and password are required" });
+  }
+  if (users.some(user => user.username === username)) {
+    return res.status(409).json({ message: "Username already exists" });
+  }
+  users.push({ username, password });
+  return res.status(201).json({ message: "User registered successfully" });
 });
 
-// Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// Route for getting the list of all books
+public_users.get('/', function (req, res) {
+  let allBooks = '';
+
+  // Loop through each book in the books object
+  for (const bookId in books) {
+    if (books.hasOwnProperty(bookId)) {
+      // Convert the book to a formatted JSON string and add to the response
+      allBooks += JSON.stringify(books[bookId], null, 2) + '\n';
+    }
+  }
+
+  return res.status(200).send(allBooks);
 });
 
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
-  
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// Route for getting book details based on ISBN
+public_users.get('/isbn/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+  if (!book) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+  return res.status(200).json(book);
 });
 
-// Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// Route for getting books by author
+public_users.get('/author/:author', function (req, res) {
+  const author = req.params.author;
+  const booksByAuthor = [];
+
+  // Loop through each book in the books object
+  for (const bookId in books) {
+    if (books.hasOwnProperty(bookId) && books[bookId].author === author) {
+      booksByAuthor.push(books[bookId]);
+    }
+  }
+
+  if (booksByAuthor.length === 0) {
+    return res.status(404).json({ message: "No books found by this author" });
+  }
+
+  return res.status(200).json(booksByAuthor);
 });
 
-//  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// Route for getting books by title
+public_users.get('/title/:title', function (req, res) {
+  const title = req.params.title;
+  const booksWithTitle = [];
+
+  // Loop through each book in the books object
+  for (const bookId in books) {
+    if (books.hasOwnProperty(bookId) && books[bookId].title.includes(title)) {
+      booksWithTitle.push(books[bookId]);
+    }
+  }
+
+  if (booksWithTitle.length === 0) {
+    return res.status(404).json({ message: "No books found with this title" });
+  }
+
+  return res.status(200).json(booksWithTitle);
+});
+
+// Route for getting book reviews based on ISBN
+public_users.get('/review/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+  if (!book) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+  const reviews = book.reviews || [];
+  return res.status(200).json(reviews);
 });
 
 module.exports.general = public_users;
